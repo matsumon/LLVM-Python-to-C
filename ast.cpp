975d7324052220ae48c1898879fe7b4cfc15dcc8
matsumon <matsumon@oregonstate.edu>
2022-06-02 13:05:14 -0700

    auto push

--- ast.cpp
+++ ast.cpp
@@ -247,7 +247,7 @@ llvm::Value * ASTAssignmentStatement::generateLLVM()const{
   llvm::Value * newRHS = rhs->generateLLVM();
   llvm::Value * newLHS = lhs->generateLLVM();
   std::string name = *lhs->name;
-  std::cout<<"Name "<<name<<" "<<newLHS<<NULL<<std::endl;
+  std::cout<<"Name "<<name<<" "<<newRHS<<NULL<<std::endl;
   return  assignmentStatement(name, newRHS);
 }
 
