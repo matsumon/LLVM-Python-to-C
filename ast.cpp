f938699e9ca95b2d46ec5cb1bdd618d1bfe5378b
matsumon <matsumon@oregonstate.edu>
2022-06-03 00:29:26 -0700

    auto push

--- ast.cpp
+++ ast.cpp
@@ -260,7 +260,7 @@ llvm::Value * ASTBoolean::generateLLVM()const{
   llvm::Value * newLHS = numericConstant((float)value);
   // newLHS = TheBuilder.CreateFCmpUGE(numericConstant((float)value), numericConstant((float)1), "gtetmp");
   return TheBuilder.CreateFCmpUGE(numericConstant((float)value), numericConstant((float)1), "gtetmp");
-  return TheBuilder.CreateUIToFP(
+  return TheBuilder.CreateFPToUI(
     newLHS,
     llvm::Type::getFloatTy(TheContext),
     "gtebooltmp"
