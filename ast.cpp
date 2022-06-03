86376a74f95d1ab3e971a5863096d1994a7db4a3
matsumon <matsumon@oregonstate.edu>
2022-06-03 00:03:36 -0700

    auto push

--- ast.cpp
+++ ast.cpp
@@ -257,7 +257,13 @@ llvm::Value * ASTInteger::generateLLVM()const{
   return numericConstant((float)value);
 }
 llvm::Value * ASTBoolean::generateLLVM()const{
-  return numericConstant((float)value);
+  newLHS = TheBuilder.CreateFCmpUGE(numericConstant((float)value), numericConstant((float)1), "gtetmp");
+  return TheBuilder.CreateUIToFP(
+    newLHS,
+    llvm::Type::getFloatTy(TheContext),
+    "gtebooltmp"
+  );
+  // return numericConstant((float)value);
 }
 llvm::Value * ASTBinaryOperatorExpression::generateLLVM()const{
   llvm::Value * newLHS = lhs->generateLLVM();
