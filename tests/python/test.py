794afa5be22a6685f5fa659dd70c1e1a23656130
matsumon <matsumon@oregonstate.edu>
2022-06-02 17:26:40 -0700

    auto push

--- tests/python/test.py
+++ tests/python/test.py
@@ -1,5 +1,17 @@
+from re import L
+
+
 return_value = 2.0
 a = 3.0
 return_value = a
 a = 5
 c = True
+g = 1 + 2
+h = 2 - 1
+i = 3 x 2
+j = 3.0 * 2.0
+k = 1/2
+l = 1 > 3
+m = 1 >= 3
+n = 1 < 3
+o = 1 <= 3
