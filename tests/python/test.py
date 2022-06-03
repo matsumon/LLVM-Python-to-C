2bf65e9bd2d0f550cd8bfa72e0e7336346110872
matsumon <matsumon@oregonstate.edu>
2022-06-03 00:33:40 -0700

    auto push

--- tests/python/test.py
+++ tests/python/test.py
@@ -1,6 +1,10 @@
 # line 399 hangs if it doesn't run, maybe block?
 # not returning i1 for condition in branches
-n = 8
-if True:
-    n = n * 2
-return_value = n
+pi = 3.1415
+r = 3
+v = 0
+if pi * r * r <= 28:
+    v = pi * r * r
+else:
+    v = pi * 2 * r
+return_value = v
