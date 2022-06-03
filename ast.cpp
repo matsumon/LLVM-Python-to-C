2f6c374fe0e42ed9aa0674ab08671d0fb6dd5d28
matsumon <matsumon@oregonstate.edu>
2022-06-02 19:22:34 -0700

    auto push

--- ast.cpp
+++ ast.cpp
@@ -257,7 +257,7 @@ llvm::Value * ASTInteger::generateLLVM()const{
   return numericConstant((float)value);
 }
 llvm::Value * ASTBoolean::generateLLVM()const{
-  return llvm::Type::getFloatTy((float)value)
+  return llvm::Type::getFloatTy((float)value);
   // return numericConstant((float)value);
 }
 llvm::Value * ASTBinaryOperatorExpression::generateLLVM()const{
