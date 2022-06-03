864dc935960832bc6f29b5eb0b43079f7c8638f1
matsumon <matsumon@oregonstate.edu>
2022-06-02 18:11:54 -0700

    auto push

--- tests/python/test.py
+++ tests/python/test.py
@@ -1,14 +1,3 @@
-return_value = 2.0
-a = 3.0
-return_value = a
-a = 5
-c = True
-g = 1 - 2
-h = 2 - 1
-i = 3 * 2
-j = 3.0 / 2.0
-l = 1 > 3
-m = 1 >= 3
-n = 1 < 3
-o = 1 <= 3
-p = 1 + 1 + 1 + 1
+return_value = 3
+if True:
+    return_value = return_value * 2
